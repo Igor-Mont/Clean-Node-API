f38d884d183d17363c07f320a81ba3ff014bd63a
Igor Mont <igornathandev@gmail.com>
2022-04-27 20:46:45 -0300

    test: ensure SignUp routes returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+import app from '../config/app'
+
+describe('Signup Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Igor',
+        email: 'igor@mail.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
+      .expect(200)
+  });
+  
+});
