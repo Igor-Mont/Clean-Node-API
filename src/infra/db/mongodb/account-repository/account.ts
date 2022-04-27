f363b484306c118b07270fe10c4367b7a7940980
Igor Mont <igornathandev@gmail.com>
2022-04-27 15:40:40 -0300

    feat: ensure AccountMongoRepository return an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,32 @@
+import { AddAccountRepository } from "../../../../data/protocols/add-account-repository";
+import { AccountModel } from "../../../../domain/models/account";
+import { AddAccountModel } from "../../../../domain/usecases/add-account";
+import { MongoHelper } from '../helpers/mongo-helper'
+
+interface AccountByID {
+  _id: any
+  name: string
+  email: string
+  password: string
+}
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+
+		const result = await accountCollection.insertOne(accountData)
+		const { insertedId: id }  = result
+		const accountById = await accountCollection.findOne({ _id: id }) as AccountByID
+		const { _id, ...accountWithoutId } = accountById;
+
+		const account = Object.assign(
+      {},
+			accountWithoutId,
+			{ id: _id?.toHexString() }
+      ) as AccountModel
+      
+		return account
+
+  }
+
+}
