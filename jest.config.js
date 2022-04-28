b98b512c05f559fdde21f8d7f44ec71c526e9dea
Igor Mont <igornathandev@gmail.com>
2022-04-27 21:37:14 -0300

    refactor: add correct connection string on MongoHelper

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   coverageProvider: 'babel',
-  collectCoverageFrom: [ '<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**' ],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**', '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
